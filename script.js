7bf7c2fc0f13447932ba7852dee2b06b37b52965
MozerBlack <joao.mozer.santos@escola.pr.gov.br>
2026-05-25 10:53:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const visor = document.getElementById('resultado');
 
 function inserir(valor) {
-    // Se tiver "Erro" na tela, limpa antes de digitar o novo número
     if (visor.value === 'Erro') {
         limparVisor();
     }
@@ -12,25 +11,19 @@ function limparVisor() {
     visor.value = '';
 }
 
-// Caso queira apagar de 1 em 1 (opcional, mas bom ter)
-function deletarUltimo() {
-    visor.value = visor.value.slice(0, -1);
-}
-
 function calcular() {
     try {
         if (visor.value !== '') {
             let expressao = visor.value;
 
-            // Traduz o que está na tela para o padrão do Javascript
+            // Transforma os caracteres visuais em operadores matemáticos reais
             expressao = expressao.replace(/×/g, '*');
             expressao = expressao.replace(/÷/g, '/');
             expressao = expressao.replace(/%/g, '/100');
 
-            // Calcula o resultado
             let resultadoFinal = eval(expressao);
 
-            // Se der um número quebrado muito longo, limita as casas decimais
+            // Evita dízimas periódicas gigantescas na tela
             if (resultadoFinal % 1 !== 0) {
                 resultadoFinal = Number(resultadoFinal.toFixed(6));
             }
